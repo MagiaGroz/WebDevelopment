03c9a2b3fd05c41455f0de4c5143e5a6c35ff10e
unknown <grozaga@gmail.com>
2020-02-12 14:37:50 +0600

    latest

---
+++ w4/my-app/src/app/product-item/product-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+  products=products;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  share(){
+    window.alert('The product has been shared!');
+  }
+
+}
